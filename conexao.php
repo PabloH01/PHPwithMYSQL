f8ae7b756c2ec70d7832bafd0660248695954dfa
PabloH01 <pablo.nadai@etec.sp.gov.br>
2020-11-27 19:16:21 -0300

    Signed-off-by: PabloH01 <pablo.nadai@etec.sp.gov.br>

--- conexao.php
+++ conexao.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:conexao.php
 <?php
 
 $host = "ec2-54-226-165-161.compute-1.amazonaws.com";
@@ -10,16 +9,3 @@ $conexao = mysqli_connect($host, $user, $pass, $banco)
         or die("Problemas com a conexão do Banco de Dados");
 mysqli_set_charset($conexao, "UTF8");
 ?>
-=======
-<?php
-
-$host = "localhost";
-$user = "root";
-$pass = "usbw";
-$banco = "banco";
-
-$conexao = mysqli_connect($host, $user, $pass, $banco)
-        or die("Problemas com a conexão do Banco de Dados");
-mysqli_set_charset($conexao, "UTF8");
-?>
->>>>>>> e644c0d3fd27f0f6baf51831717d425af3305c32:trabalhoimagens/conexao.php
