91e359194c2aebee26aa927d24cb0f9b23e2e76d
PabloH01 <pablo.nadai@etec.sp.gov.br>
2020-11-30 18:30:20 -0300

    Versão84

--- imagens.sql
+++ imagens.sql
@@ -6,6 +6,6 @@ SET time_zone = "-03:00";
 CREATE TABLE IF NOT EXISTS `imagens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `arquivo` varchar(100) NOT NULL,
-  `nomeImagem` varchar(90) NOT NULL,
+  `nomeImagem` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
