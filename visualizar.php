036c182b3ee3eeb8dbf8a73afbdeb1422e019687
PabloH01 <pablo.nadai@etec.sp.gov.br>
2020-11-30 13:36:23 -0300

    Versão62

--- visualizar.php
+++ visualizar.php
@@ -2,5 +2,5 @@
 
 include_once('conexao.php');
 $id = $_GET['id'];
-echo "<img src='arquivos/" . $dados['arquivo'] . "' width='100px' heigth='100px'>";
+echo "<img src='arquivos/" . $dados['arquivo'] . "' width='500px' heigth='500px'>";
 ?>
