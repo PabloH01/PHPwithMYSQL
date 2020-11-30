228223cde585e87bf230b9b0b3dbaa2143524fe4
PabloH01 <pablo.nadai@etec.sp.gov.br>
2020-11-30 11:03:37 -0300

    Versão40

--- upload.php
+++ upload.php
@@ -11,10 +11,10 @@ else if ($tipografia == 2){
     $nome_final = ".jpg";
 }
 
-if (substr($_FILES['arquivo']['nomeImagem'], -3) == "png" || substr($_FILES['arquivo']['nomeImagem'], -3) == "jpg") {
+if (substr($_FILES['arquivo']['name'], -3) == "png" || substr($_FILES['arquivo']['name'], -3) == "jpg") {
     $dir = './arquivos/';
     $tmpName = $_FILES['arquivo']['tmp_name'];
-    $name = $_FILES['arquivo']['nomeImagem'];
+    $name = $_FILES['arquivo']['name'];
     // move_uploaded_file
     if (move_uploaded_file($tmpName, $dir . $nome_final)) {
          $sqlstring = "insert into imagens (id, arquivo, nomeImagem) values (null, '$nome_final', 'nomeImagem')";
